e97396202e1586bb7f912480ba87c782afaf8c10
mazkatraz <mazkatraz@yahoo.com>
2017-12-02 23:03:16 -0500

    Added the showme command

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
+var XmlHttp = require('xmlhttprequest')
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -61,8 +62,71 @@ bot.on('message', function(user, userID, channelID, message, evt) {
 						message: "Well hello there, " + user + '.'
 					});
 				}
+                break;
+            case 'showme':
+                var offset = getRandomIntInclusive(1, 100);
+                var url = "https://www.googleapis.com/customsearch/v1?" +
+                             "key=" + auth.customSearchKey +
+                             "&cx=" + auth.searchEngineId +
+                             "&start=" + offset +
+                             "&q=painting&searchType=image";
+                
+                httpGetAsync(url, function(response){
+                    //logger.info(getRandomPainting(response));
+                    bot.sendMessage({
+                        to: channelID,
+                        message: getRandomPainting(response)
+                    });
+                })
+                break;
                 // Just add any case commands if you want to..
         }
     }
 
 });
+
+/**
+ * Get a random Bob Ross painting url.
+ * @param {String} response - String json
+ * @return {String} result - Painting URL or error message
+ */
+function getRandomPainting(response) {
+    var result = "There are no mistakes, only happy accidents.";
+    try {
+        var json = JSON.parse(response);
+        var randomIndex = getRandomIntInclusive(1, 10);
+        var result = json.items[randomIndex].link;
+    } catch (err) {
+        logger.info(err);
+    }
+    
+    return result;
+}
+
+/**
+ * Gets a random number between two numbers inclusively
+ * @param {Number} min - min inclusive number
+ * @param {Number} max - max inclusive number
+ * @return {Number} result - random number between min and max inclusively
+ */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+/**
+ * Makes an async call to an URL
+ * @param {String} theUrl
+ * @param {Function} callback - callback function
+ */
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XmlHttp.XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
